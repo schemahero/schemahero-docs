ab384e9908245573a37db57ef2920b07c86d4a9a
Marc Campbell <marc.e.campbell@gmail.com>
2026-02-04 12:13:01 +0000

    Use SchemaHero logo instead of emoji in docs header
    Replace the 🦸 emoji with the actual schemahero-logo.svg image
    in the documentation header.
    
    Signed-off-by: Marc Campbell <marc.e.campbell@gmail.com>

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,8 +3,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
-    <span style={{ fontWeight: 700, fontSize: '1.2rem' }}>
-      🦸 SchemaHero
+    <span style={{ display: 'flex', alignItems: 'center', gap: '8px', fontWeight: 700, fontSize: '1.2rem' }}>
+      <img src="/images/schemahero-logo.svg" alt="SchemaHero" style={{ height: '28px', width: '28px' }} />
+      SchemaHero
     </span>
   ),
   project: {
